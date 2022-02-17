0a3d4b53b727249318d13f54e85e8a4d27d14484
tomcraig99 <tomcraig99@gmail.com>
2022-02-17 13:28:15 -0600

    version 1.0

--- RPSclass.py
+++ RPSclass.py
@@ -1,13 +1,13 @@
 import random
 
-class opponent:
+class Opponent:
 
     def __init__(self):
         self.opponent = 'rock'
 
     def opponentChoice(self):
         num = random.randint(1,3)
-        if num == 3:
+        if num == 1:
             self.opponent = 'rock'
         if num == 2:
             self.opponent = 'paper'
@@ -18,19 +18,24 @@ class opponent:
         return self.opponent
 
 
-class player:
+class Player:
     
     def __init__(self):
         self.player = 'rock'
 
-        def playerChoice(self):
-            num = random.randint(1,3)
-            if num == 1:
-                self.player = 'rock'
-            if num == 2:
-                self.player = 'paper'
-            if num == 3:
-                self.player = 'scissors'
+    def playerChoice(self):
+        choice=0
+        try:
+            while choice not in range(1,4):
+                choice = int(input('1 = Rock, 2 = Paper, 3 = Scissors '))
+        except:
+            pass
+        if choice == 1:
+            self.player = 'rock'
+        if choice == 2:
+            self.player = 'paper'
+        if choice == 3:
+            self.player = 'scissors'
         
     def getPlayer(self):
         return self.player
